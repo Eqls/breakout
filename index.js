30af8a984866a6c175287c33fd5edf8298df13c2
Armandas <argnow12@gmail.com>
2018-12-06 09:20:01 +0700

    fixed fps bug, also made fixed fps rate

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
+const GAME_FPS = 60
+
 let canvas = document.getElementById('gameCanvas')
 let ctx = canvas.getContext('2d')
-let timer = new Date().getMilliseconds()
+let timer = Date.now()
 let fps = 0
 let displayFps = 0
+let x = canvas.width / 2
+let y = canvas.height - 30
+let dx = 1
+let dy = 0
+let ballRadius = 10
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  fpsCounter()
+  drawFpsCounter()
+  drawBall()
+  x += dx
+  y += dy
+}
+
+function drawBall() {
   ctx.beginPath()
-  ctx.arc(50, 50, 10, 0, Math.PI * 2)
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
   ctx.fillStyle = '#0095DD'
   ctx.fill()
   ctx.closePath()
 }
 
-setInterval(draw, 10)
-
-function fpsCounter() {
-  if (new Date().getMilliseconds() - timer > 0) {
+function drawFpsCounter() {
+  console.log(`${Date.now()}  ${timer}`)
+  if (Date.now() - timer < 1000) {
     fps++
   } else {
-    timer = new Date().getMilliseconds()
+    timer = Date.now()
     displayFps = fps
     fps = 0
   }
@@ -30,3 +42,5 @@ function fpsCounter() {
   ctx.fillText(displayFps === 0 ? fps : displayFps, 10, 20)
   ctx.closePath()
 }
+
+setInterval(draw, 1000 / GAME_FPS)
