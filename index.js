6b86856eb439d469f81af8d1ce5eb92607a2517d
Armandas <argnow12@gmail.com>
2018-12-06 08:51:36 +0700

    added game loop and fps counter

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-var canvas = document.getElementById('gameCanvas')
-var ctx = canvas.getContext('2d')
+let canvas = document.getElementById('gameCanvas')
+let ctx = canvas.getContext('2d')
+let timer = new Date().getMilliseconds()
+let fps = 0
+let displayFps = 0
 
-ctx.beginPath()
-ctx.rect(20, 40, 50, 50)
-ctx.fillStyle = '#FF0000'
-ctx.fill()
-ctx.closePath()
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  fpsCounter()
+  ctx.beginPath()
+  ctx.arc(50, 50, 10, 0, Math.PI * 2)
+  ctx.fillStyle = '#0095DD'
+  ctx.fill()
+  ctx.closePath()
+}
+
+setInterval(draw, 10)
+
+function fpsCounter() {
+  if (new Date().getMilliseconds() - timer > 0) {
+    fps++
+  } else {
+    timer = new Date().getMilliseconds()
+    displayFps = fps
+    fps = 0
+  }
+  ctx.beginPath()
+  ctx.font = '20px Arial'
+  ctx.fillStyle = 'gold'
+  ctx.fillText(displayFps === 0 ? fps : displayFps, 10, 20)
+  ctx.closePath()
+}
